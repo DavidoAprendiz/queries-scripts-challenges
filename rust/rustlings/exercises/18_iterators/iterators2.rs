033d573d4aafb0a640f232e30b806d571081fc3a
davidoaprendiz <21132833+DavidoAprendiz@users.noreply.github.com>
2024-07-10 13:11:29 +0100

    Add Rust exercises

--- rust/rustlings/exercises/18_iterators/iterators2.rs
+++ rust/rustlings/exercises/18_iterators/iterators2.rs
@@ -6,33 +6,28 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-// Step 1.
-// Complete the `capitalize_first` function.
-// "hello" -> "Hello"
 pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => format!("{}{}", first.to_uppercase(), (input[1..].to_string())),
     }
 }
 
-// Step 2.
-// Apply the `capitalize_first` function to a slice of string slices.
-// Return a vector of strings.
-// ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    let mut my_vec = vec![];
+    for word in words {
+        my_vec.push(capitalize_first(word))
+    }
+    my_vec
 }
 
-// Step 3.
-// Apply the `capitalize_first` function again to a slice of string slices.
-// Return a single string.
-// ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let mut my_str = String::from("");
+    for word in words {
+        my_str.push_str(&capitalize_first(word))
+    }
+    my_str
 }
 
 #[cfg(test)]
